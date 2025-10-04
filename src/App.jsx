09dbdb44c2fbf38c5d70b90caa1f5c48c05e6118
David Ogungbemi <daveedog2003@gmail.com>
2025-10-04 23:44:37 +0100

    added a favicon

--- src/App.jsx
+++ src/App.jsx
@@ -40,19 +40,19 @@ function App() {
   };
   return (
     <>
-      <header className="w-full bg-gray-200 py-2 px-6 lg:max-h-15 border-b border-gray-300">
+      <header className="w-full bg-gray-200 py-2 px- border-b border-gray-300">
         <img
           src="/images/donezo-logo.png"
-          className="logo max-h-[4rem] md:max-h-[5rem] mx-auto"
+          className="logo max-h-[2rem] mx-auto"
           alt="Donezo logo"
         />
       </header>
-      <main className="w-full h-full max-w-3xl mx-auto p-4 lg:grid lg:grid-cols-[320px_1fr]">
-        <aside className="hidden lg:block bg-gray">
-          <ul className="fixed bottom-0 left-1/2 transform -translate-x-1/2 bg-gray-200 w-full max-w-2xl flex justify-around items-center gap-4 text-white p-4 rounded-tl-md rounded-tr-md shadow-md">
+      <main className="w-full h-full p-4 lg:grid lg:grid-cols-[320px_1fr]">
+        <aside className="hidden h-full lg:block bg-gray">
+          <ul className=" bg-gray-200 w-full h-full flex flex-col justify-start items-start gap-4 text-white p-4 rounded-tl-md rounded-tr-md shadow-md">
             <li
-              className={`text-xs text-black cursor-pointer px-4 py-2 w-full text-center border-r border-gray-400 ${
-                isActiveTab === 0 ? "font-bold" : ""
+              className={`text-base text-black cursor-pointer px-4 py-2 w-full text-center ${
+                isActiveTab === 0 ? "font-bold bg-gray-50" : ""
               }`}
               role="menuitem"
               aria-roledescription="Go to TodoApp"
@@ -61,8 +61,8 @@ function App() {
               TodoApp
             </li>
             <li
-              className={`text-xs text-black cursor-pointer px-4 py-2 w-full text-center ${
-                isActiveTab === 1 ? "font-bold" : ""
+              className={`text-base text-black cursor-pointer px-4 py-2 w-full text-center ${
+                isActiveTab === 1 ? "font-bold bg-gray-50" : ""
               }`}
               role="menuitem"
               aria-roledescription="Go to Travel List"
@@ -71,8 +71,8 @@ function App() {
               Travel List
             </li>
             <li
-              className={`text-xs text-black cursor-pointer px-4 py-2 w-full text-center border-l border-gray-400 ${
-                isActiveTab === 2 ? "font-bold" : ""
+              className={`text-base text-black cursor-pointer px-4 py-2 w-full text-center ${
+                isActiveTab === 2 ? "font-bold bg-gray-50" : ""
               }`}
               role="menuitem"
               aria-roledescription="Go to NotesApp"
@@ -82,7 +82,7 @@ function App() {
             </li>
           </ul>
         </aside>
-        <section className="mt-4 lg:mt-0 lg:ml-4">
+        <section className="mt-4 lg:mt-0 lg:ml-4 max-w-3xl mx-auto">
           {isActiveTab === 0 && <TodoApp />}
           {isActiveTab === 1 && <TravelList />}
           {isActiveTab === 2 && <NotesApp />}
--- src/components/snackBar.jsx
+++ src/components/snackBar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const SnackBar = ({ onSetActiveTab, isActiveTab }) => {
   return (
-    <ul className="fixed bottom-0 left-1/2 transform -translate-x-1/2 bg-gray-200 w-full max-w-2xl flex justify-around items-center gap-4 text-white p-4 rounded-tl-md rounded-tr-md shadow-md">
+    <ul className="fixed bottom-0 left-1/2 transform -translate-x-1/2 bg-gray-200 w-full max-w-2xl flex justify-around items-center gap-4 text-white p-4 rounded-tl-md rounded-tr-md shadow-md lg:hidden">
       <li
         className={`text-xs text-black cursor-pointer px-4 py-2 w-full text-center border-r border-gray-400 ${
           isActiveTab === 0 ? "font-bold" : ""
